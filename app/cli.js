9bdfa6edf1042a5b5e482d3f5c915b127e05cfc8
Michael Clayton <mclayton@redhat.com>
2017-09-21 18:22:27 -0400

    add 'init' parameter

--- app/cli.js
+++ app/cli.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').argv;
+const fs   = require('fs');
 const path = require('path');
+
+const argv = require('yargs').argv;
 
 const spandx = require('./spandx');
 const config = require('./config');
+
+if (argv._.includes('init')) {
+    const sampleConfigPath = path.resolve(__dirname, '../spandx.config.js');
+    const sampleConfig = fs.readFileSync(sampleConfigPath);
+    console.log(sampleConfig.toString());
+    process.exit(0);
+}
 
 const confArg = argv.c || argv.config || 'spandx.config.js';
 
