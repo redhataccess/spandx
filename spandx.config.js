3eedf1e29fb82dd56ed7409e558365384b9fdb33
Michael Clayton <mclayton@redhat.com>
2017-09-21 18:20:58 -0400

    make the sample config file functional

--- spandx.config.js
+++ spandx.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    host: 'local.my-app.com',
+    host: 'localhost',
     port: 1337,
     verbose: true,
     routes: {
@@ -12,12 +12,12 @@ module.exports = {
         // Route a URL path to an app server.
         // This is most useful for testing local files (esp JS and CSS) against
         // a remote QA or production server.
-        '/': { host: 'http://production.my-app.com' },
+        '/': { host: 'https://access.redhat.com' },
 
         // Route a URL path to an app server, and watch local files for changes.
         // This is most useful for putting a local development at a certain
         // path on your spandx server.  Includes browser-sync auto-reloading.
-        '/': { host: 'http://localhost:8080/', watch: '~/projects/my-app' },
+        // '/': { host: 'http://localhost:8080/', watch: '~/projects/my-app' },
 
     },
 };
