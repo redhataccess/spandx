addde0d6ca76c9aa9bbb36270e354ec1f2744479
Michael Clayton <mclayton@redhat.com>
2019-04-29 17:41:17 -0400

    fix remote route logging of [object Object] (#56)
    Travis still can't run tests, but they work in node 8/9/10/11.

--- app/spandx.js
+++ app/spandx.js
@@ -88,20 +88,21 @@ async function init(confIn) {
         console.log(
             _.map(conf.webRoutes, route => {
                 return conf.spandxUrl
-                    .map(
-                        url =>
-                            `  ${c.fg.l.blue}${url
-                                .replace(/\/$/, "")
-                                .replace(
-                                    new RegExp(`${conf.startPath}$`),
-                                    ""
-                                )}${c.end}${c.fg.l.green}${route[0]}${
-                                c.e
-                            } will be routed to ${c.fg.l.blue}${route[1].host
-                                .default || route[1].host}${c.e}${
-                                c.fg.l.green
-                            }${route[0]}${c.e}`
-                    )
+                    .map(url => {
+                        const env = _.findKey(conf.host, host =>
+                            new RegExp(`${host}`).test(url)
+                        );
+
+                        return `  ${c.fg.l.blue}${url
+                            .replace(/\/$/, "")
+                            .replace(new RegExp(`${conf.startPath}$`), "")}${
+                            c.end
+                        }${c.fg.l.green}${route[0]}${c.e} will be routed to ${
+                            c.fg.l.blue
+                        }${route[1].host[env] || route[1].host}${c.e}${
+                            c.fg.l.green
+                        }${route[0]}${c.e}`;
+                    })
                     .join("\n");
             }).join("\n")
         );
